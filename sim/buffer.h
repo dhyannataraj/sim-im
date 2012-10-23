7e94ed7f22c22549abe5966fe12cca4aae8948d8
noragen <noragen@gmx.net>
2012-10-23 17:23:05 +0200

    * Visual Studio 2012 Support + Adaptions

--- sim/buffer.h
+++ sim/buffer.h
@@ -23,16 +23,16 @@
 
 #include "simapi.h"
 
-#ifdef HAVE_STDINT_H
-	#include <stdint.h>
-#else
+//#ifdef HAVE_STDINT_H
+#include <cstdint>
+/*#else
 	typedef signed		char	int8_t;
 	typedef signed		short	int16_t;
 	typedef signed		long	int32_t;
 	typedef unsigned	char	uint8_t;
 	typedef unsigned	short	uint16_t;
 	typedef unsigned	long	uint32_t; 
-#endif
+#endif*/
 
 class EXPORT Buffer : public QByteArray
 {
